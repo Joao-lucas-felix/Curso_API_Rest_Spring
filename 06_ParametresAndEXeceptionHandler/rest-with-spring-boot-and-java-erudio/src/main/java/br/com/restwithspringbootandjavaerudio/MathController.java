9b43af805332747252c3cfa6d4c789861795d2b8
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-04-11 08:33:19 -0300

    Implementação dos outros métodos da calculadora

--- 06_ParametresAndEXeceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/restwithspringbootandjavaerudio/MathController.java
+++ 06_ParametresAndEXeceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/restwithspringbootandjavaerudio/MathController.java
@@ -13,13 +13,54 @@ public class MathController {
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne, //Recupera os dados do Path
             @PathVariable(value = "numberTwo") String numberTwo
-    ) throws Exception {
+    ){
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
             throw  new UnsuportMathOperationExeception("Please set a numeric value");
         }
 
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
+
+    @GetMapping(path = "/sub/{numberOne}/{numberTwo}")
+    public Double sub(
+             @PathVariable(value = "numberOne") String numberOne,
+             @PathVariable(value = "numberTwo") String numberTwo
+    ){
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+    @GetMapping(path = "/mult/{numberOne}/{numberTwo}")
+    public Double mult(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ){
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+    @GetMapping(path = "/div/{numberOne}/{numberTwo}")
+    public Double div(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    )
+    {
+        Double divisor = convertToDouble(numberTwo);
+        if(divisor == 0 ) throw new UnsuportMathOperationExeception("Divison by Zero");
+        return convertToDouble(numberOne)/divisor;
+    }
+    @GetMapping(path = "/average/{numberOne}/{numberTwo}")
+    public Double average(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ){
+        return (convertToDouble(numberOne)+convertToDouble(numberTwo))/2;
+    }
+    @GetMapping(path = "/sqrt/{number}")
+    public Double sqrt(
+            @PathVariable(value = "number") String number
+    ){
+        return Math.sqrt(convertToDouble(number));
+    }
+
+
+
 
     private Double convertToDouble(String strNumber) {
         if (strNumber == null) throw  new UnsuportMathOperationExeception("Please set a numeric value");
