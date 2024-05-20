5b9e66225d57b69d499541451bed7ee0cd96f666
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-05-20 11:16:19 -0300

    CORS implementation + Integration tests

--- 15_CrossOriginResourcesSharing/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/restwithspringbootandjavaerudio/config/WebConfig.java
+++ 15_CrossOriginResourcesSharing/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/restwithspringbootandjavaerudio/config/WebConfig.java
@@ -1,10 +1,13 @@
 package br.com.restwithspringbootandjavaerudio.config;
 
 import br.com.restwithspringbootandjavaerudio.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,10 +15,22 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType aplicationYml = MediaType.parseMediaType("application/x-yaml");
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(","); // pega os valores lidos do resources
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST", "PUT") // indica para quais verbos http o cors  vai funcionar.
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)// indica que as origins permitidas são as que nos lemos
+                .allowCredentials(true); // para permitir autenticação
     }
 
     @Override
--- 15_CrossOriginResourcesSharing/rest-with-spring-boot-and-java-erudio/src/test/java/config/TestsConfigs.java
+++ 15_CrossOriginResourcesSharing/rest-with-spring-boot-and-java-erudio/src/test/java/config/TestsConfigs.java
@@ -3,4 +3,11 @@ package config;
 public class TestsConfigs {
     //para indicar para o container qual é a server port
     public static final int SERVER_PORT = 8888;
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_YML = "application/x-yaml";
+    public static final String VALID_ORIGIN = "http://localhost:4200";
+    public static final String INVALID_ORIGIN = "http://localhost:2";
 }
